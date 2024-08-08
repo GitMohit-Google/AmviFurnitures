a28d557f7b56b7efb8f64ff879377b7b8a43e793
GitMohit123 <jindalm55@gmail.com>
2024-08-08 19:36:38 +0530

    UI changes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,214 +1,283 @@
-import AspectRatio from '@mui/joy/AspectRatio';
-import Card from '@mui/joy/Card';
-import Typography from '@mui/joy/Typography';
+import AspectRatio from "@mui/joy/AspectRatio";
+import Card from "@mui/joy/Card";
+import Typography from "@mui/joy/Typography";
 import React from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Cards = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className='flex flex-row w-full p-3 px-5 items-center justify-evenly mt-10'>
-      <Link to="/Plastic Dinner Table">
-      <Card sx={{
-          width: 320,
-          backgroundColor: '#2a2a2a',
-          cursor:"pointer",
-          transition: 'opacity 0.3s ease',
-          '&:hover': {
-            opacity: 0.9,
-          },
-        }}>
-        <div style={{ textAlign: 'center' }}>
-          <Typography sx={{ color: 'white',fontSize:"30px" }} variant="h6">Premium Tables</Typography>
-          <Typography sx={{ color: 'white' }} variant="body2">Office</Typography>
-        </div>
-        <AspectRatio minHeight="300px" maxHeight="500px">
-          <img
-            src="/banners/Table-Photoroom.png"
-            loading="lazy"
-            alt=""
-            className="bg-[#2a2a2a]"
-          />
-        </AspectRatio>
-      </Card>
-          </Link>
-
-      <Link to="/Premium Chair">
-      <Card sx={{
-          width: 320,
-          backgroundColor: '#2a2a2a',
-          cursor:"pointer",
-          transition: 'opacity 0.3s ease',
-          '&:hover': {
-            opacity: 0.9,
-          },
-        }}>
-        <div style={{ textAlign: 'center' }}>
-          <Typography sx={{ color: 'white',fontSize:"30px" }} variant="h6">Signature Chairs</Typography>
-          <Typography sx={{ color: 'white' }} variant="body2">Office</Typography>
-        </div>
-        <AspectRatio minHeight="300px" maxHeight="500px">
-
-          <img
-            src="/banners/SignatureChair-Photoroom.png"
-            loading="lazy"
-            alt=""
-            className="bg-[#2a2a2a]"
-          />
-        </AspectRatio>
-      </Card>
-          </Link>
-          <Link to="/Multipurpose Stool">
-      <Card sx={{
-          width: 320,
-          backgroundColor: '#2a2a2a',
-          cursor:"pointer",
-          transition: 'opacity 0.3s ease',
-          '&:hover': {
-            opacity: 0.9,
-          },
-        }}>
-        <div style={{ textAlign: 'center' }}>
-          <Typography sx={{ color: 'white',fontSize:"30px" }} variant="h6">Multipurpose Stools</Typography>
-          <Typography sx={{ color: 'white' }} variant="body2">Office</Typography>
-        </div>
-        <AspectRatio minHeight="300px" maxHeight="500px">
-          <img
-            src="/banners/Stool-Photoroom.png"
-            loading="lazy"
-            alt=""
-            className="bg-[#2a2a2a]"
-          />
-        </AspectRatio>
-      </Card>
-          </Link>
-
-          <Link to="/Double-Top Table">
-      <Card sx={{
-          width: 320,
-          backgroundColor: '#2a2a2a',
-          cursor:"pointer",
-          transition: 'opacity 0.3s ease',
-          '&:hover': {
-            opacity: 0.9,
-          },
-        }}>
-        <div style={{ textAlign: 'center' }}>
-          <Typography sx={{ color: 'white',fontSize:"30px" }} variant="h6">Double Tap Table</Typography>
-          <Typography sx={{ color: 'white' }} variant="body2">Office</Typography>
-        </div>
-        <AspectRatio minHeight="300px" maxHeight="500px">
-            <img
-              src="/banners/DoubleTable-Photoroom.png"
-              loading="lazy"
-              alt=""
-              className="bg-[#2a2a2a]"
-            />
-        </AspectRatio>
-      </Card>
-          </Link>
-    </div>
-    <div className='flex flex-row w-full p-3 px-5 items-center justify-evenly mb-10'>
-    <Link to="/Plastic Dinner Table">
-    <Card sx={{
-        width: 320,
-        backgroundColor: '#2a2a2a',
-        cursor:"pointer",
-        transition: 'opacity 0.3s ease',
-        '&:hover': {
-          opacity: 0.9,
-        },
-      }}>
-      <div style={{ textAlign: 'center' }}>
-        <Typography sx={{ color: 'white',fontSize:"30px" }} variant="h6">Premium Tables</Typography>
-        <Typography sx={{ color: 'white' }} variant="body2">Office</Typography>
-      </div>
-      <AspectRatio minHeight="300px" maxHeight="500px">
-        <img
-          src="/banners/Table-Photoroom.png"
-          loading="lazy"
-          alt=""
-          className="bg-[#2a2a2a]"
-        />
-      </AspectRatio>
-    </Card>
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="flex flex-row w-full p-3 px-5 items-center justify-evenly mt-10">
+        <Link to="/Plastic Dinner Table">
+          <Card
+            sx={{
+              width: 320,
+              backgroundColor: "#2a2a2a",
+              cursor: "pointer",
+              transition: "opacity 0.3s ease",
+              "&:hover": {
+                opacity: 0.9,
+              },
+            }}
+          >
+            <div style={{ textAlign: "center" }}>
+              <Typography
+                sx={{ color: "white", fontSize: "30px" }}
+                variant="h6"
+              >
+                Premium Tables
+              </Typography>
+              <Typography sx={{ color: "white" }} variant="body2">
+                Office
+              </Typography>
+            </div>
+            <AspectRatio minHeight="300px" maxHeight="500px">
+              <img
+                src="/banners/Table-Photoroom.png"
+                loading="lazy"
+                alt=""
+                className="bg-[#2a2a2a]"
+              />
+            </AspectRatio>
+          </Card>
         </Link>
 
-    <Link to="/Premium Chair">
-    <Card sx={{
-        width: 320,
-        backgroundColor: '#2a2a2a',
-        cursor:"pointer",
-        transition: 'opacity 0.3s ease',
-        '&:hover': {
-          opacity: 0.9,
-        },
-      }}>
-      <div style={{ textAlign: 'center' }}>
-        <Typography sx={{ color: 'white',fontSize:"30px" }} variant="h6">Signature Chairs</Typography>
-        <Typography sx={{ color: 'white' }} variant="body2">Office</Typography>
-      </div>
-      <AspectRatio minHeight="300px" maxHeight="500px">
-
-        <img
-          src="/banners/SignatureChair-Photoroom.png"
-          loading="lazy"
-          alt=""
-          className="bg-[#2a2a2a]"
-        />
-      </AspectRatio>
-    </Card>
+        <Link to="/Premium Chair">
+          <Card
+            sx={{
+              width: 320,
+              backgroundColor: "#2a2a2a",
+              cursor: "pointer",
+              transition: "opacity 0.3s ease",
+              "&:hover": {
+                opacity: 0.9,
+              },
+            }}
+          >
+            <div style={{ textAlign: "center" }}>
+              <Typography
+                sx={{ color: "white", fontSize: "30px" }}
+                variant="h6"
+              >
+                Signature Chairs
+              </Typography>
+              <Typography sx={{ color: "white" }} variant="body2">
+                Office
+              </Typography>
+            </div>
+            <AspectRatio minHeight="300px" maxHeight="500px">
+              <img
+                src="/banners/SignatureChair-Photoroom.png"
+                loading="lazy"
+                alt=""
+                className="bg-[#2a2a2a]"
+              />
+            </AspectRatio>
+          </Card>
         </Link>
         <Link to="/Multipurpose Stool">
-    <Card sx={{
-        width: 320,
-        backgroundColor: '#2a2a2a',
-        cursor:"pointer",
-        transition: 'opacity 0.3s ease',
-        '&:hover': {
-          opacity: 0.9,
-        },
-      }}>
-      <div style={{ textAlign: 'center' }}>
-        <Typography sx={{ color: 'white',fontSize:"30px" }} variant="h6">Multipurpose Stools</Typography>
-        <Typography sx={{ color: 'white' }} variant="body2">Office</Typography>
+          <Card
+            sx={{
+              width: 320,
+              backgroundColor: "#2a2a2a",
+              cursor: "pointer",
+              transition: "opacity 0.3s ease",
+              "&:hover": {
+                opacity: 0.9,
+              },
+            }}
+          >
+            <div style={{ textAlign: "center" }}>
+              <Typography
+                sx={{ color: "white", fontSize: "30px" }}
+                variant="h6"
+              >
+                Multipurpose Stools
+              </Typography>
+              <Typography sx={{ color: "white" }} variant="body2">
+                Office
+              </Typography>
+            </div>
+            <AspectRatio minHeight="300px" maxHeight="500px">
+              <img
+                src="/banners/Stool-Photoroom.png"
+                loading="lazy"
+                alt=""
+                className="bg-[#2a2a2a]"
+              />
+            </AspectRatio>
+          </Card>
+        </Link>
+        <Link to="/Armless Chair">
+          <Card
+            sx={{
+              width: 320,
+              backgroundColor: "#2a2a2a",
+              cursor: "pointer",
+              transition: "opacity 0.3s ease",
+              "&:hover": {
+                opacity: 0.9,
+              },
+            }}
+          >
+            <div style={{ textAlign: "center" }}>
+              <Typography
+                sx={{ color: "white", fontSize: "30px" }}
+                variant="h6"
+              >
+                Armless Chairs
+              </Typography>
+              <Typography sx={{ color: "white" }} variant="body2">
+                Office
+              </Typography>
+            </div>
+            <AspectRatio minHeight="300px" maxHeight="500px">
+              <img
+                src="/banners/Armless-Chair-Photoroom.png"
+                loading="lazy"
+                alt=""
+                className="bg-[#2a2a2a]"
+              />
+            </AspectRatio>
+          </Card>
+        </Link>
+        
       </div>
-      <AspectRatio minHeight="300px" maxHeight="500px">
-        <img
-          src="/banners/Stool-Photoroom.png"
-          loading="lazy"
-          alt=""
-          className="bg-[#2a2a2a]"
-        />
-      </AspectRatio>
-    </Card>
+      <div className="flex flex-row w-full p-3 px-5 items-center justify-evenly mb-10">
+        <Link to="/Comfy Chair">
+          <Card
+            sx={{
+              width: 320,
+              backgroundColor: "#2a2a2a",
+              cursor: "pointer",
+              transition: "opacity 0.3s ease",
+              "&:hover": {
+                opacity: 0.9,
+              },
+            }}
+          >
+            <div style={{ textAlign: "center" }}>
+              <Typography
+                sx={{ color: "white", fontSize: "30px" }}
+                variant="h6"
+              >
+                Comfy Chairs
+              </Typography>
+              <Typography sx={{ color: "white" }} variant="body2">
+                Office
+              </Typography>
+            </div>
+            <AspectRatio minHeight="300px" maxHeight="500px">
+              <img
+                src="/banners/Alpha-Chair-Photoroom.png"
+                loading="lazy"
+                alt=""
+                className="bg-[#2a2a2a]"
+              />
+            </AspectRatio>
+          </Card>
         </Link>
 
-        <Link to="/Double-Top Table">
-    <Card sx={{
-        width: 320,
-        backgroundColor: '#2a2a2a',
-        cursor:"pointer",
-        transition: 'opacity 0.3s ease',
-        '&:hover': {
-          opacity: 0.9,
-        },
-      }}>
-      <div style={{ textAlign: 'center' }}>
-        <Typography sx={{ color: 'white',fontSize:"30px" }} variant="h6">Double Tap Table</Typography>
-        <Typography sx={{ color: 'white' }} variant="body2">Office</Typography>
-      </div>
-      <AspectRatio minHeight="300px" maxHeight="500px">
-          <img
-            src="/banners/DoubleTable-Photoroom.png"
-            loading="lazy"
-            alt=""
-            className="bg-[#2a2a2a]"
-          />
-      </AspectRatio>
-    </Card>
+        <Link to="/Mid-Back Chair">
+          <Card
+            sx={{
+              width: 320,
+              backgroundColor: "#2a2a2a",
+              cursor: "pointer",
+              transition: "opacity 0.3s ease",
+              "&:hover": {
+                opacity: 0.9,
+              },
+            }}
+          >
+            <div style={{ textAlign: "center" }}>
+              <Typography
+                sx={{ color: "white", fontSize: "30px" }}
+                variant="h6"
+              >
+                Mid-Back Chairs
+              </Typography>
+              <Typography sx={{ color: "white" }} variant="body2">
+                Office
+              </Typography>
+            </div>
+            <AspectRatio minHeight="300px" maxHeight="500px">
+              <img
+                src="/banners/Mid-Back-Photoroom.png"
+                loading="lazy"
+                alt=""
+                className="bg-[#2a2a2a]"
+              />
+            </AspectRatio>
+          </Card>
         </Link>
-  </div>
+        <Link to="/Double-Top Table">
+          <Card
+            sx={{
+              width: 320,
+              backgroundColor: "#2a2a2a",
+              cursor: "pointer",
+              transition: "opacity 0.3s ease",
+              "&:hover": {
+                opacity: 0.9,
+              },
+            }}
+          >
+            <div style={{ textAlign: "center" }}>
+              <Typography
+                sx={{ color: "white", fontSize: "30px" }}
+                variant="h6"
+              >
+                Double Tap Table
+              </Typography>
+              <Typography sx={{ color: "white" }} variant="body2">
+                Office
+              </Typography>
+            </div>
+            <AspectRatio minHeight="300px" maxHeight="500px">
+              <img
+                src="/banners/DoubleTable-Photoroom.png"
+                loading="lazy"
+                alt=""
+                className="bg-[#2a2a2a]"
+              />
+            </AspectRatio>
+          </Card>
+        </Link>
+        <Link to="/Baby Chair">
+          <Card
+            sx={{
+              width: 320,
+              backgroundColor: "#2a2a2a",
+              cursor: "pointer",
+              transition: "opacity 0.3s ease",
+              "&:hover": {
+                opacity: 0.9,
+              },
+            }}
+          >
+            <div style={{ textAlign: "center" }}>
+              <Typography
+                sx={{ color: "white", fontSize: "30px" }}
+                variant="h6"
+              >
+                Baby Chairs
+              </Typography>
+              <Typography sx={{ color: "white" }} variant="body2">
+                Office
+              </Typography>
+            </div>
+            <AspectRatio minHeight="300px" maxHeight="500px">
+              <img
+                src="/banners/Teddy-Chair-Photoroom.png"
+                loading="lazy"
+                alt=""
+                className="bg-[#2a2a2a]"
+              />
+            </AspectRatio>
+          </Card>
+        </Link>
+      </div>
     </div>
   );
 };
--- src/sections/ProductsSection.jsx
+++ src/sections/ProductsSection.jsx
@@ -4,14 +4,14 @@ import ClientStats from "./ClientStats";
 
 const ProductsSection = () => {
   return (
-    <div className="flex w-full items-center p-3 px-5 flex-col">
+    <div className="flex w-full items-center py-3 flex-col">
       <div className="flex w-full items-center justify-center text-6xl p-3">
-        <p className="w-[750px] text-center font-semibold">
+        <p className="w-[750px] text-center font-medium">
           Quality and Guaranteed Products Here
         </p>
       </div>
       <Cards />
-      <div className="flex flex-col w-full px-6 gap-10">
+      <div className="flex flex-col w-full px-9 gap-10">
         <img src="/banners/Banner.png" alt="" className="rounded-lg shadow-lg"/>
         {/* <img src="/Demo.png" alt="" className="rounded-lg shadow-lg"/> */}
       </div>
