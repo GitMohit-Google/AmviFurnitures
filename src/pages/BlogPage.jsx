1ea0816ccccbac5178b11e498d141177474b3c18
GitMohit123 <jindalm55@gmail.com>
2024-08-11 19:02:54 +0530

    window

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Mosaic from '../components/mosaic'
 
 const BlogPage = () => {
-  
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to top when the component mounts
+  }, []);
   return (
     <div className='overflow-auto p-8 px-8 w-full flex flex-col gap-10 justify-center items-center'>
       <p className='text-4xl sm:text-6xl font-semibold text-center'><span className='text-[#ee4d37]'>Latest</span> Updates and Articles</p>
